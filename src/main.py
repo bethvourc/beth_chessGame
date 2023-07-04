891b41bc076f1f1c4da3a98c3f305bb0893da957
Bethvour Chike <bethvourc@gmail.com>
2023-07-03 21:36:42 -0400

    show possible moves for knights

--- src/main.py
+++ src/main.py
@@ -23,6 +23,7 @@ class Main:
 
         while True:
             game.show_bg(screen)
+            game.show_moves(screen)
             game.show_pieces(screen)
 
             if dragger.dragging:
@@ -40,14 +41,21 @@ class Main:
                 # if clicked square has a piece
                 if board.squares[clicked_row][clicked_col].has_piece():
                    piece = board.squares[clicked_row][clicked_col].piece
+                   board.calc_moves(piece, clicked_row, clicked_col)
                    dragger.save_initial(event.pos)
                    dragger.drag_piece(piece)
+                   # show methods
+                   game.show_bg(screen)
+                   game.show_moves(screen)
+                   game.show_pieces(screen)
                
                elif event.type == pygame.MOUSEMOTION:
                 # check if piece is actually being dragged
                 if dragger.dragging:
                     dragger.update_mouse(event.pos)
+                    # show methods
                     game.show_bg(screen)
+                    game.show_moves(screen)
                     game.show_pieces(screen)
                     dragger.update_blit(screen)
                
