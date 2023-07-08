bf564d7d4e468ce6299c07da44b8716659dbaf43
Bethvour Chike <bethvourc@gmail.com>
2023-07-08 18:00:03 -0400

    enabled piece movement and capturing of enemy piece

--- src/main.py
+++ src/main.py
@@ -4,6 +4,8 @@ import sys
 
 from const import *
 from game import Game
+from square import Square
+from move import Move
 
 
 class Main:
@@ -60,6 +62,26 @@ class Main:
                     dragger.update_blit(screen)
                
                elif event.type == pygame.MOUSEBUTTONUP:
+                    
+
+                    if dragger.dragging:
+                       dragger.update_mouse(event.pos)
+
+                       released_row = dragger.mouseY // SQSIZE
+                       released_col = dragger.mouseX // SQSIZE
+
+                       # create possible move
+                       initial = Square(dragger.initial_row, dragger.initial_col)
+                       final = Square(released_row, released_col)
+                       move = Move(initial, final)
+
+                        # valid move?
+                       if board.valid_move(dragger.piece, move):
+                          board.move(dragger.piece, move)
+                          # draw
+                          game.show_bg(screen)
+                          game.show_pieces(screen)
+
                     dragger.undrag_piece()
                
                elif event.type == pygame.QUIT:
