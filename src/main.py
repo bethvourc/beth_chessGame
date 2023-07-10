09c96ef855d3d25446e32ac7a0ce3f4f3b4c3070
Bethvour Chike <bethvourc@gmail.com>
2023-07-09 22:28:41 -0400

    created show hover method

--- src/main.py
+++ src/main.py
@@ -29,6 +29,8 @@ class Main:
             game.show_moves(screen)
             game.show_pieces(screen)
 
+            game.show_hover(screen)
+
             if dragger.dragging:
                dragger.update_blit(screen)
 
@@ -55,14 +57,21 @@ class Main:
                         game.show_pieces(screen)
                
                elif event.type == pygame.MOUSEMOTION:
-                # check if piece is actually being dragged
-                if dragger.dragging:
-                    dragger.update_mouse(event.pos)
-                    # show methods
-                    game.show_bg(screen)
-                    game.show_moves(screen)
-                    game.show_pieces(screen)
-                    dragger.update_blit(screen)
+                    motion_row = event.pos[1] // SQSIZE
+                    motion_col = event.pos[0] // SQSIZE
+
+                    game.set_hover(motion_row, motion_col)
+
+                    # check if piece is actually being dragged
+                    if dragger.dragging:
+                        dragger.update_mouse(event.pos)
+                        # show methods
+                        game.show_bg(screen)
+                        game.show_last_move(screen)
+                        game.show_moves(screen)
+                        game.show_pieces(screen)
+                        game.show_hover(screen)
+                        dragger.update_blit(screen)
                
                elif event.type == pygame.MOUSEBUTTONUP:
                     
