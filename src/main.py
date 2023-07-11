591d7caa847b4aca0c1644ff7f62095be77845f1
Bethvour Chike <bethvourc@gmail.com>
2023-07-11 16:53:14 -0400

    Added the sound functionality

--- src/main.py
+++ src/main.py
@@ -89,7 +89,11 @@ class Main:
 
                         # valid move?
                        if board.valid_move(dragger.piece, move):
+                          captured = board.squares[released_row][released_col].has_piece()
+
                           board.move(dragger.piece, move)
+                          # sound
+                          game.play_sound(captured)
                           # draw
                           game.show_bg(screen)
                           game.show_last_move(screen)
