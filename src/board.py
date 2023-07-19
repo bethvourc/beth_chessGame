f21552e463842b4677862c4b88096b3b5018d546
Bethvour Chike <bethvourc@gmail.com>
2023-07-18 20:38:19 -0400

    working on en passant capture

--- src/board.py
+++ src/board.py
@@ -17,14 +17,29 @@ class Board:
         initial = move.initial
         final = move.final
 
+        en_passant_empty = self.squares[final.row][final.col].isempty()
+
         # console board move update
         self.squares[initial.row][initial.col].piece = None
         self.squares[final.row][final.col].piece = piece
-
+           
 
         # Pawn promotion
         if isinstance(piece, Pawn):
-            self.check_promotion(piece, final)
+            # en passant capture
+            diff = final.col - initial.col
+            if diff != 0 and en_passant_empty():
+                # console board move update
+                self.squares[initial.row][initial.col + diff].piece = None
+                self.squares[final.row][final.col].piece = piece
+
+            # pawn en passant
+            if self.en_passant(initial, final):
+                piece.en_passant  = True
+
+            else:
+                # pawn promotion
+                self.check_promotion(piece, final)
 
         # King castling 
         if isinstance(piece, King):
@@ -53,6 +68,9 @@ class Board:
 
     def castling(self, initial, final):
         return abs(initial.col - final.col) == 2
+    
+    def en_passant(self, initial, final):
+        return abs(initial.row - final.row) == 2
     
     # create method to detect check/checkmate 
     def in_check(self, piece, move):
@@ -122,6 +140,51 @@ class Board:
                         else:
                             # append new move 
                             piece.add_move(move)
+
+
+            # en passant moves
+            r = 3 if piece.color == "white" else 4
+            fr = 2 if piece.color == "white" else 5
+            # left en passant 
+            if Square.in_range(col - 1) and row == r:
+                if self.squares[row][col-1].has_enemy_piece(piece.color):
+                    p = self.squares[row][col-1].piece
+                    if isinstance(p, Pawn):
+                        if p.en_passant:
+                            # create initial and final move squares 
+                            initial = Square(row, col)
+                            final = Square(fr, col-1, p)
+                            # create a new move 
+                            move = Move(initial, final)
+                            # check if moves leads to self check
+                            if bool:
+                                if not self.in_check(piece, move):
+                                    # append new move 
+                                    piece.add_move(move)
+                            else:
+                                # append new move 
+                                piece.add_move(move)
+
+            # right en passant 
+            if Square.in_range(col+1) and row == r:
+                if self.squares[row][col+1].has_enemy_piece(piece.color):
+                    p = self.squares[row][col+1].piece
+                    if isinstance(p, Pawn):
+                        if p.en_passant:
+                            # create initial and final move squares 
+                            initial = Square(row, col)
+                            final = Square(fr, col+1, p)
+                            # create a new move 
+                            move = Move(initial, final)
+                            # check if moves leads to self check
+                            if bool:
+                                if not self.in_check(piece, move):
+                                    # append new move 
+                                    piece.add_move(move)
+                            else:
+                                # append new move 
+                                piece.add_move(move)
+
 
         def knight_moves():
             possible_moves = [
